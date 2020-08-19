30b2b5ffb59975ceadfd933d80e1843fa81f90c3
vagrant <vagrant@localhost.localdomain>
2020-08-19 06:09:28 +0000

    ログイン後の画面作成、users&booksコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
   devise_for :users
   root 'homes#top'
   get 'homes/about'
